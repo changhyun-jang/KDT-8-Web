f1c882db79dbb0a4a3b95d87838b75d8e6548a65
JCH <ckdgusjang@naver.com>
2023-10-11 13:12:55 +0900

    10_11_80일차

---
+++ editor/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Editor from "./editablePage";
+
+function App() {
+  return (
+    <>
+      <Editor />
+    </>
+  );
+}
+
+export default App;
---
+++ editor/src/editableBlock.tsx
@@ -0,0 +1,6 @@
+import ContentEditable from "react-contenteditable";
+import { useState, useEffect, useRef } from "react";
+
+export default function EditableBlock() {
+  return;
+}
